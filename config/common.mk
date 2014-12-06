68ccbdc47ebcba772819c746c3dc6bfeed7dd502
Mazda <mazdarider23@gmail.com>
2014-12-06 06:20:50 -0500

    Initial commit of new DU vendor, based on CM vendor setup
    Change-Id: I224ea7110660a6ecc4c32e129cecd45e90b400b4

---
+++ config/common.mk
@@ -0,0 +1,119 @@
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-google
+else
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.com.android.dataroaming=false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
+
+# Disable excessive dalvik debug messages
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.debug.alloc=0
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
+    vendor/du/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/du/prebuilt/common/bin/50-du.sh:system/addon.d/50-du.sh \
+    vendor/du/prebuilt/common/bin/99-backup.sh:system/addon.d/99-backup.sh \
+    vendor/du/prebuilt/common/etc/backup.conf:system/etc/backup.conf
+
+# Signature compatibility validation
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/bin/otasigcheck.sh:system/bin/otasigcheck.sh
+
+# Init file
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/etc/init.local.rc:root/init.du.rc
+
+# Copy latinime for gesture typing
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+
+# SELinux filesystem labels
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/etc/init.d/50selinuxrelabel:system/etc/init.d/50selinuxrelabel
+
+# Enable SIP+VoIP on all targets
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Don't export PS1 in /system/etc/mkshrc.
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/etc/mkshrc:system/etc/mkshrc \
+    vendor/du/prebuilt/common/etc/sysctl.conf:system/etc/sysctl.conf
+
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/du/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit \
+    vendor/du/prebuilt/common/bin/sysinit:system/bin/sysinit
+
+# Required packages
+PRODUCT_PACKAGES += \
+    CellBroadcastReceiver \
+    Development \
+    SpareParts
+
+# Optional packages
+PRODUCT_PACKAGES += \
+    Basic \
+    PhaseBeam
+
+# DSPManager
+PRODUCT_PACKAGES += \
+    DSPManager \
+    libcyanogen-dsp \
+    audio_effects.conf
+
+# Extra Optional packages
+PRODUCT_PACKAGES += \
+    LatinIME \
+    BluetoothExt \
+    DashClock
+
+# Extra tools
+PRODUCT_PACKAGES += \
+    openvpn \
+    e2fsck \
+    mke2fs \
+    tune2fs \
+    mount.exfat \
+    fsck.exfat \
+    mkfs.exfat
+
+# easy way to extend to add more packages
+-include vendor/extra/product.mk
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/du/overlay/common
+
+# Versioning System
+ANDROID_VERSION = 5.01
+DU_VERSION = v9.0
+
+ifndef DU_BUILD_TYPE
+    DU_BUILD_TYPE := UNOFFICIAL
+    PLATFORM_VERSION_CODENAME := UNOFFICIAL
+endif
+
+# Set all versions
+DU_VERSION := DU_$(DU_BUILD)_$(ANDROID_VERSION)_$(shell date -u +%Y%m%d-%H%M).$(DU_VERSION)-$(DU_BUILD_TYPE)
+DU_MOD_VERSION := DU_$(DU_BUILD)_$(ANDROID_VERSION)_$(shell date -u +%Y%m%d-%H%M).$(DU_VERSION)-$(DU_BUILD_TYPE)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    BUILD_DISPLAY_ID=$(BUILD_ID) \
+    ro.du.version=$(DU_VERSION) \
+    ro.modversion=$(DU_BUILD_TYPE)-$(DU_VERSION) \
